0ca3379003bccc6b0703aa084ec8bd4c6a03e2e9
Bereket Girma <bereketgirma2472@gmail.com>
2021-09-22 11:32:37 +0300

    modifying the script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get upgrade -y
 sudo apt-get install -y curl 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
