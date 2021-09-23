0af1b9fe27d70374d1c3aeafccfc6d97fb92a459
Bereket Girma <bereketgirma2472@gmail.com>
2021-09-23 14:35:10 +0300

    update index.js

--- index.js
+++ index.js
@@ -2,23 +2,45 @@ var http = require("http");
 var sys = require('sys')
 var exec = require('child_process').exec;
 var url = require("url");
+const execSync = require('child_process').execSync;
 
 function onRequest(request, response) {
-    var params = url.parse(request.url,true).query;
-    var comm = "";
-    console.log(params.command);
-   if(params.command == 'TO_SPEAKER')
-	comm = "pacmd set-sink-port 0 analog-output-speaker";
-   else if (params.command == 'TO_HEADPHONE' )
-	comm = "pacmd set-sink-port 0 analog-output-headphones";
-    const execSync = require('child_process').execSync;
-    // import { execSync } from 'child_process';  // replace ^ if using ES modules
-    const output = execSync(comm, { encoding: 'utf-8' });  // the default is 'buffer'
-    //console.log('Output was:\n', output);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    if(!output)response.end("Changing playback Device Successful");
-    else  response.end("unable to change the playback device");
-   
+    try {
+        response.setHeader('Access-Control-Allow-Origin', 'https://ayo.ju.edu.et:8880');
+        response.setHeader('Access-Control-Request-Method', 'OPTIONS, GET');
+        response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        response.setHeader('Access-Control-Allow-Headers', 'Origin');
+
+        var params;
+        var command;
+        params = url.parse(request.url, true).query;
+
+        switch (params.command) {
+            case 'STATUS':
+                return response.end(JSON.stringify({ status: true, message: "OK" }));
+            case 'TO_SPEAKER':
+                command = "pacmd set-sink-port 0 analog-output-speaker";
+                break;
+            case 'TO_HEADPHONE':
+                command = "pacmd set-sink-port 0 analog-output-headphones";
+                break;
+            default:
+                return response.end(JSON.stringify({ status: false, message: "Some Kind of Error Occurred" }));
+        }
+
+        const output = execSync(command, { encoding: 'utf-8' });  // the default is 'buffer'
+
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        if (!output || output === 'undefined')
+            response.end(JSON.stringify({ status: true, message: "Changing playback Device Successful" }));
+        else
+            response.end(JSON.stringify({ status: false, message: "unable to change the playback device" }));
+    } catch (error) {
+        return response.end(JSON.stringify({ status: false, message: "Error Occurred" }));
+
+    }
+
+
 }
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(10023);
